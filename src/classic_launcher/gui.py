47a1e3fd476182f221e7a3baf035f488e7ea80c4
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-02-08 13:10:12 +0000

    Add CSS handling; more boilerplate/tutorial code

--- src/classic_launcher/gui.py
+++ src/classic_launcher/gui.py
@@ -1,19 +1,40 @@
 import sys
-from typing import Any
 
 import gi
 
+from classic_launcher.config import APP_CONFIG
+from classic_launcher.const import APP_ID
+
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Adw, Gtk  # ty:ignore[unresolved-import]  # noqa: E402
+from gi.repository import Adw, Gdk, Gtk  # ty:ignore[unresolved-import]  # noqa: E402
 
-from classic_launcher.const import APP_ID  # noqa: E402
+css_provider = Gtk.CssProvider()
+css_provider.load_from_path(APP_CONFIG.stylesheet_file)
+Gtk.StyleContext.add_provider_for_display(
+    Gdk.Display.get_default(),
+    css_provider,
+    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+)
 
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        # Things will go here
+        self.set_default_size(600, 250)
+        self.set_title("Classic Launcher")
+        self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(self.box1)
+
+        self.button = Gtk.Button(label="Hello")
+        self.label = Gtk.Label(label="Hi!")
+        self.label.set_css_classes(["title"])
+        self.box1.append(self.button)
+        self.box1.append(self.label)
+        self.button.connect("clicked", self.hello)
+
+    def hello(self, button):
+        print("Hi!")
 
 
 class MyApp(Adw.Application):
